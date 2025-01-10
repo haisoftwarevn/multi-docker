bec8b1351891af6359abfbd1baacad7d9ad219a5
haisoftwarevn <haisoftwarevn@gmail.com>
2025-01-10 11:14:18 +0700

    t

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,25 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import React , {Component} from 'react';
-// import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
-// import OtherPage from './OtherPage';
-// import Fib from './Fib';
+import logo from './logo.svg';
+import './App.css';
+import React , {Component} from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
-
-// function App() {
-//   return (
-  
-//     <Router>
-//         <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <Link to="/">Home</Link>
-//       </header>
-//       <div>
-//         <Route exact path="/" component = {Fib} />
-//         <Route exact path="/otherpage" component = {OtherPage} />
-//       </div>
-//     </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import OtherPage from "./OtherPage";
-import Fib from "./Fib";
 
 function App() {
   return (
+  
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
-        <Routes>
-          <Route path="/" element={<Fib />} />
-          <Route path="/otherpage" element={<OtherPage />} />
-        </Routes>
+        <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/">Home</Link>
+      </header>
+      <div>
+        <Route exact path="/" component = {Fib} />
+        <Route exact path="/otherpage" component = {OtherPage} />
       </div>
+    </div>
     </Router>
   );
 }
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,155 +1,83 @@
-// import React, {Component} from "react";
-// import axios from 'axios'
+import React, {Component} from "react";
+import axios from 'axios'
 
-// class Fib extends Component {
+class Fib extends Component {
 
-//     state = {
-//         seenIndexes: [],
-//         values : {},
-//         index: ''
-//     }
-
-//     componentDidMount(){
-//         this.fetchValues()
-//         this.fetIndexes()
-//     }
-
-//     async fetchValues(){
-//         const values = await axios.get('/api/values/current')
-//         this.setState({values: values.data})
-//     }
-
-//     async fetIndexes(){
-//         const seenIndexes = await axios.get('/api/values/all')
-//         this.setState({
-//             seenIndexes: seenIndexes.data
-//         })
-//     }
-
-//     renderSeenIndexes(){
-//         return this.state.seenIndexes.map(({number}) => number).join(', ')
-//     }
-
-//     renderValues(){
-
-//         const entries = []
-
-//         for(let key in this.state.values){
-//             entries.push(
-//                 <div key={key}>
-
-//                     For index {key}, I calculated {this.state.values[key]}
-//                 </div>
-//             )
-//         }
-
-//         return entries
-//     }
-
-//     handleSubmit= async (event) => {
-//         event.preventDefault()
-
-//         await axios.post('/api/values',{
-//             index: this.state.index
-//         })
-
-//         this.setState({index: ''})
-//     }
-//     render(){
-
-//         return (
-//             <div>
-//                 <form onSubmit={this.handleSubmit}>
-//                     <label>Enter your index</label>
-//                     <input
-
-//                         value={this.state.index}
-//                         onChange={event => this.setState({
-//                             index: event.target.value
-//                         })}
-                    
-//                     />
-//                     <button>Submit</button>
-//                 </form>
-//                 <h3>Indexes I have seen</h3>
-//                 {this.renderSeenIndexes()}
-//                 <h3>Calculated Values</h3>
-//                 {this.renderValues()}
-//             </div>
-//         )
-//     }
-// }
-
-// export default Fib
-
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-const Fib = () => {
-  const [seenIndexes, setSeenIndexes] = useState([]);
-  const [values, setValues] = useState({});
-  const [index, setIndex] = useState("");
-
-  useEffect(() => {
-    fetchValues();
-    fetchIndexes();
-  }, []);
-
-  const fetchValues = async () => {
-    const values = await axios.get("/api/values/current");
-    setValues(values.data);
-  };
-
-  const fetchIndexes = async () => {
-    const seenIndexes = await axios.get("/api/values/all");
-    setSeenIndexes(seenIndexes.data);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    await axios.post("/api/values", {
-      index: index,
-    });
-    setIndex("");
-  };
-
-  const renderSeenIndexes = () => {
-    return seenIndexes.map(({ number }) => number).join(", ");
-  };
-
-  const renderValues = () => {
-    const entries = [];
-
-    for (let key in values) {
-      entries.push(
-        <div key={key}>
-          For index {key} I calculated {values[key]}
-        </div>
-      );
+    state = {
+        seenIndexes: [],
+        values : {},
+        index: ''
     }
 
-    return entries;
-  };
+    componentDidMount(){
+        this.fetchValues()
+        this.fetIndexes()
+    }
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>Enter your index:</label>
-        <input
-          value={index}
-          onChange={(event) => setIndex(event.target.value)}
-        />
-        <button>Submit</button>
-      </form>
+    async fetchValues(){
+        const values = await axios.get('/api/values/current')
+        this.setState({values: values.data})
+    }
 
-      <h3>Indexes I have seen:</h3>
-      {renderSeenIndexes()}
+    async fetIndexes(){
+        const seenIndexes = await axios.get('/api/values/all')
+        this.setState({
+            seenIndexes: seenIndexes.data
+        })
+    }
 
-      <h3>Calculated Values:</h3>
-      {renderValues()}
-    </div>
-  );
-};
+    renderSeenIndexes(){
+        return this.state.seenIndexes.map(({number}) => number).join(', ')
+    }
 
-export default Fib;
+    renderValues(){
+
+        const entries = []
+
+        for(let key in this.state.values){
+            entries.push(
+                <div key={key}>
+
+                    For index {key}, I calculated {this.state.values[key]}
+                </div>
+            )
+        }
+
+        return entries
+    }
+
+    handleSubmit= async (event) => {
+        event.preventDefault()
+
+        await axios.post('/api/values',{
+            index: this.state.index
+        })
+
+        this.setState({index: ''})
+    }
+    render(){
+
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter your index</label>
+                    <input
+
+                        value={this.state.index}
+                        onChange={event => this.setState({
+                            index: event.target.value
+                        })}
+                    
+                    />
+                    <button>Submit</button>
+                </form>
+                <h3>Indexes I have seen</h3>
+                {this.renderSeenIndexes()}
+                <h3>Calculated Values</h3>
+                {this.renderValues()}
+            </div>
+        )
+    }
+}
+
+export default Fib
